6f4ec39f9baedb820e61a13c7ea12bf9298821b8
Mikhail.Shamin <munk13@gmail.com>
2010-09-05 00:16:20 +0400

    Fixed alias mapper

--- library/MusicBrainz/Adapter/Rest/Mapper/Alias.php
+++ library/MusicBrainz/Adapter/Rest/Mapper/Alias.php
@@ -14,21 +14,9 @@ class Munk_MusicBrainz_Adapter_Rest_Mapper_Alias extends Munk_MusicBrainz_Adapte
     
     /**
      * 
-     * @var string
-     */
-    //protected $_resultXPath = '/metadata/artist';
-    
-    /**
-     * 
-     * @var string
-     */
-    //protected $_resultSetXPath = '/metadata/artist-list/artist';
-    
-    /**
-     * 
      * @var array
      */
     protected $_map = array(
-        'name'           => '',
+        'name' => '/.',
     );
 }
